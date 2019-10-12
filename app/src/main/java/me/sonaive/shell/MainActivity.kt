47eaae0c1d5944ec06cf0c5ab11840e3ce861c8e
taoliuh <taoliuh@gmail.com>
2019-10-12 08:41:00 +0800

    创建一个空的壳子app, 尽量移除了各种依赖并配置了混淆文件及gradle.

---
+++ app/src/main/java/me/sonaive/shell/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.sonaive.shell
+
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
